4c1f257d16814951d619e6f01e3ba1c78ab6b4e2
Matt McMeans <mrmcmans@memphis.edu>
2023-12-05 22:16:53 -0600

    fix intro

--- app/src/Intro.js
+++ app/src/Intro.js
@@ -1,23 +1,21 @@
 import memoji from './memoji.png'
-import Gradient from './Gradient.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Intro() {
     return (
-        <Container id="intro" className='intro'>
+        <Container id="intro" className='intro' style={{width: '75%'}}>
             <Row className="justify-content-md-center">
-                <Col md={{ span: 4, offset: 2 }} className='hello'>Hello! I am <span className='name'>Matthew McMeans</span></Col>
-                <Col className='memoji-container'><img className="me" src={memoji} alt="memoji"/></Col>
+                <Col md={7}  className="align-self-center" style={{ marginTop: '100px'}}>
+                <div className='hello' style={{ margin: '20px 0 20px 0'}}>Hello! I am <span className='name'>Matthew McMeans</span></div>
+                <div className='im-se'>Im a Software Engineer.</div>
+                <div className="currently" >Currently, I'm a software engineer intern at <a className="dataprofits" href="https://data-profits.com/">Data Profits</a></div>
+                </Col>
+                <Col className='memoji-container' style={{ marginTop: '100px'}}><img className="me" src={memoji} alt="memoji"/></Col>
             </Row>
-            <Row className='im-se'>
-                <Col>Im a Software Engineer.</Col>
-            </Row>
-            <Row className="currently">
-                <Col>Currently, I'm a software engineer intern at <a className="dataprofits" href="https://data-profits.com/">Data Profits</a></Col>
-            </Row>
-            <Row className='about'>
+
+            <Row className='about' style={{ marginTop: '50px',}}>
                 <Col className='about-text'>I’m a computer science student focused on frontend development with a flair for full-stack work. My strength lies in grasping user needs and aligning them with business goals, creating tech solutions that merge both seamlessly.</Col>
             </Row>
         </Container>
