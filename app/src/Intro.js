805635b9e6672e12ca96ac39991d842154874e64
Matt McMeans <mrmcmans@memphis.edu>
2023-12-06 17:34:17 -0600

    Update styling

--- app/src/Intro.js
+++ app/src/Intro.js
@@ -6,13 +6,13 @@ import Col from 'react-bootstrap/Col';
 function Intro() {
     return (
         <Container id="intro" className='intro' style={{width: '75%'}}>
-            <Row className="justify-content-md-center">
+            <Row className="justify-content-center">
                 <Col md={7}  className="align-self-center" style={{ marginTop: '100px'}}>
                 <div className='hello' style={{ margin: '20px 0 20px 0'}}>Hello! I am <span className='name'>Matthew McMeans</span></div>
                 <div className='im-se'>Im a Software Engineer.</div>
                 <div className="currently" >Currently, I'm a software engineer intern at <a className="dataprofits" href="https://data-profits.com/">Data Profits</a></div>
                 </Col>
-                <Col className='memoji-container' style={{ marginTop: '100px'}}><img className="me" src={memoji} alt="memoji"/></Col>
+                <Col className='memoji-container' style={{ marginTop: '100px' }}><img className="me" src={memoji} alt="memoji" style={{ width: '400px'}}/></Col>
             </Row>
 
             <Row className='about' style={{ marginTop: '50px',}}>
